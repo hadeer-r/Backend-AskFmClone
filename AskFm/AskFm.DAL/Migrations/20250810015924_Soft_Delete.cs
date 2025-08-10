8ce0cc1e6e89d19785da7e5ce995928d3a92b30c
hadeer <hadderramadan7@gmail.com>
2025-08-10 05:10:35 +0300

    Add Migrations of Soft Delete

---
+++ AskFm/AskFm.DAL/Migrations/20250810015924_Soft_Delete.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AskFm.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Soft_Delete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Users",
+                type: "DATETIME",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Users",
+                type: "BIT",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "ThreadLikes",
+                type: "DATETIME",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "ThreadLikes",
+                type: "BIT",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Thread",
+                type: "DATETIME",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Thread",
+                type: "BIT",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "SavedThreads",
+                type: "DATETIME",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "SavedThreads",
+                type: "BIT",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Notifications",
+                type: "DATETIME",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Notifications",
+                type: "BIT",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Follows",
+                type: "DATETIME",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Follows",
+                type: "BIT",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Comments",
+                type: "DATETIME",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Comments",
+                type: "BIT",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "CommentLikes",
+                type: "DATETIME",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "CommentLikes",
+                type: "BIT",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "ThreadLikes");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "ThreadLikes");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Thread");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Thread");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "SavedThreads");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "SavedThreads");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Follows");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Follows");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "CommentLikes");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "CommentLikes");
+        }
+    }
+}
