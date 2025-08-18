47f9e2bcc01f2ed435a8a1f1f7b9bd3b8aab9f3f
Mahmoud Ayman <mahmoud.ayman.fcai@gmail.com>
2025-08-18 17:26:16 +0300

    Implement NotificationDto

---
+++ AskFm/AskFm.BLL/DTO/NotificationDto.cs
@@ -0,0 +1,13 @@
+public class NotificationDto
+{
+    public int Id { get; set; }
+    public string Type { get; set; }
+
+    public string Message { get; set; }
+
+    public bool IsRead { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public int ResourceId { get; set; }
+    public int UserId { get; set; }
+}
---
+++ AskFm/AskFm.BLL/Hub/NotificationHub.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AskFm.BLL.Hub
+{
+    public class NotificationHub : Microsoft.AspNetCore.SignalR.Hub
+    {
+        
+    }
+}
